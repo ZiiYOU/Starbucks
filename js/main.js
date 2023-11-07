50502cbfa30a438d41ebd96b620b731edb573ba2
Jiyoung <jiyoung990602@gmail.com>
2023-11-07 16:10:55 +0900

    Start

---
+++ js/main.js
@@ -0,0 +1,153 @@
+const searchEl = document.querySelector('.search');
+const searchinputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click',function(){
+    searchinputEl.focus();
+});
+
+searchinputEl.addEventListener('focus',function(){
+    searchEl.classList.add('focused');
+    searchinputEl.setAttribute('placeholder','통합검색');
+});
+
+searchinputEl.addEventListener('blur',function(){ // 포커스 빠졌을 때
+    searchEl.classList.remove('focused');
+    searchinputEl.setAttribute('placeholder','');
+});
+
+const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top')
+
+window.addEventListener('scroll',_.throttle(function(){
+    console.log(window.scrollY);
+    if(window.scrollY>500){
+        //배지 숨기기
+        //gsap.to(요소, 지속시간, 옵션);
+        gsap.to(badgeEl, .6, {
+            opacity:0, 
+            display:'none'
+        });
+        // 버튼 보이기
+        gsap.to(toTopEl,.2,{
+            x:0
+        });
+    }else{
+        //배지 보이기
+        gsap.to(badgeEl, .6, {
+            opacity:1,
+            display:'block'
+        });
+        // 버튼 숨기기
+        gsap.to(toTopEl,.2,{
+            x:100
+        });
+    }
+},300));
+// _.throttle(함수,시간)
+
+toTopEl.addEventListener('click',function(){
+    gsap.to(window,.7,{
+        scrollTo:0
+    })
+});
+
+const fadeEls=document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl,index) {
+    //gsap.to(요소, 지속시간, 옵션)
+    gsap.to(fadeEl,1,{
+        delay: (index+1)* .7, // 0.7 1.4 2.1 2.8
+        opacity:1
+    });
+});
+
+// new Swiper(선택자,옵션)
+new Swiper('.notice-line .swiper-container',{
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});
+
+// new Swiper(선택자,옵션)
+new Swiper('.promotion .swiper-container',{
+    slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 보인다.
+    loop: true, // 반복 재생 여부
+    pagination : { // 페이지 번호 사용 여부
+        el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+        clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+    },
+    navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+        prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
+        nextEl: '.promotion .swiper-next', // 다음 버튼 선택자
+    }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click',function(){
+    isHidePromotion = !isHidePromotion;
+    if(isHidePromotion){
+        //숨김처리
+        promotionEl.classList.add('hide');
+    }else{
+        //보임처리
+        promotionEl.classList.remove('hide');
+    }
+})
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min,max){
+    // 'toFixed()를 통해 반환된 문자 데이터를 'parseFloat()'을 통해 소수점을 가지는 숫자 데이터로 변환 
+    return parseFloat((Math.random() * (max-min) +min).toFixed(2))
+}
+
+function floatingObeject(selector,delay,size){
+    gsap.to(
+        selector, //선택자
+        random(1.5,2.5), // 애니메이션 동작 시간
+        { //옵션
+            y:size,
+            repeat: -1, // -1 은 무한반복
+            yoyo: true, //한번 동작했던 것을 반대로 동작하겠다. 이걸 넣어주면 위로도 올라온다.
+            ease: Power1.easeInOut, // easing
+            delay: random(0,delay)
+        }
+    )
+}
+
+floatingObeject('.floating1',1,15);
+floatingObeject('.floating2',5,15);
+floatingObeject('.floating3',1.5,20);
+
+// ScrollMagic Cdn
+// 요소가 화면에 보여짐 여부에 따른 요소 관리
+
+// 관리할 요소를 선택
+const spyEls = document.querySelectorAll('section.scroll-spy')
+// 요소들 반복 처리
+spyEls.forEach(function(spyEl){
+    new ScrollMagic
+    .Scene({ // 감시할 장면(Scene)을 추가
+        triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+        triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부를 감시
+    })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스를 추가
+    .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
+})
+
+new Swiper('.awards .swiper-container',{
+    slidesPerView: 5,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: true,
+    navigation: {
+        prevEl: '.awards .swiper-prev',
+        nextEl: '.awards .swiper-next'
+    }
+});
+
+const thisYearEl = document.querySelector('.this-year');
+const now = new Date();
+thisYearEl.textContent = now.getFullYear();
---
+++ js/youtube.js
@@ -0,0 +1,25 @@
+// Youtube Iframe API를 비동기로 로드합니다.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// onYouTubePlayerAPIReady 함수 이름은
+// Youtube IFrame Player API 에서 사용하는 이름이기 대문에 다르게 지정하면 동작하지 않습니다. 그리고 함수는 전역(히ㅐㅠ미)등록해야합니다!
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    videoId: 'An6LvWQuj_8', // 최초재생할 유튜브 영상 아이디
+    playerVars: {
+        autoplay: true, // 자동재생 유무
+        loop: true, // 반복재생 유무
+        playlist: 'An6LvWQuj_8' //반복재생할 유튜브 영상 아이디 목록
+    },
+    events: {
+        // 영상이 준비되었을 때
+      'onReady': function (event) {
+        event.target.mute() // 음소거!
+      }
+    }
+  });
+}
+
